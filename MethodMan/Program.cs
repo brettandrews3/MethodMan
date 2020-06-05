7e23769945762b95215dbd3c155544754c54a581
Brett Andrews <brettandrews3@gmail.com>
2020-06-05 15:47:20 -0500

    Name Q&A works

--- MethodMan/Program.cs
+++ MethodMan/Program.cs
@@ -4,13 +4,19 @@ namespace MethodMan
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello handsome! What's your name");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Good day to you, {name}!");
         }
     }
 }
 /* Okay, peep this out: this exercise ties in with the Methods prompt from
  * TrueCoders. First up: the C# program that takes user input and creates a 
  * Mad Libs story. It should read user inputs and use concatenation in its
- * Console.WriteLine or return commands.
+ * Console.WriteLine or return commands. */
