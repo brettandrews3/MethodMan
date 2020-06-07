69b253aba06f4fadfff2c5b352325479aa1f337d
Brett Andrews <brettandrews3@gmail.com>
2020-06-07 18:06:26 -0500

    Story works; commented, moved to class

--- MethodMan/Program.cs
+++ MethodMan/Program.cs
@@ -8,17 +8,7 @@ namespace MethodMan
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello handsome! What's your name?");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Good day to you, {name}!");
-
-            Console.WriteLine($"Next up, {name}: name a color that you like:");
-            var color = Console.ReadLine();
-            Console.WriteLine($"Your color is {color}. Got it. Next...");
-
-            Console.WriteLine("What's your favorite type of animal?");
-            var animal = Console.ReadLine();
-            Console.WriteLine($"You like {animal}s, too? Awesome!");
+            
         }
     }
 }
---
+++ MethodMan/Story.cs
@@ -0,0 +1,24 @@
+﻿/*using System;
+namespace MethodMan
+{
+    public class Story
+    {
+        Console.WriteLine("Hello handsome! What's your name?");
+            var name = Console.ReadLine();
+        Console.WriteLine($"Good day to you, {name}!");
+
+            Console.WriteLine($"Next up, {name}: name a color that you like:");
+            var color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}. Got it. Next...");
+
+            Console.WriteLine("What's your favorite type of animal?");
+            var animal = Console.ReadLine();
+        Console.WriteLine($"You like {animal}s, too? Awesome!");
+
+            Console.WriteLine($"Okay, {name}: here's your one-sentence story:");
+            Console.WriteLine($"My buddy {name} likes their {animal}s {color} like the ground.");
+            Console.WriteLine("************");
+            Console.WriteLine("The End!");
+    }
+}
+*/
