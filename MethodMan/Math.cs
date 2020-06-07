aaf4a9f100ac4d5f239dc3e8e407ed805be0ce1c
Brett Andrews <brettandrews3@gmail.com>
2020-06-07 18:40:46 -0500

    Sum doesn't work yet

---
+++ MethodMan/Math.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MethodMan
+{
+    public class Math
+    {
+        public static int Num1 { get; set; }
+        public static int Num2 { get; set; }
+        public object Sum(int sum) => Num1 + Num2;
+    }
+}
--- MethodMan/Program.cs
+++ MethodMan/Program.cs
@@ -4,12 +4,36 @@ namespace MethodMan
 {
     class Program
     {
-
+        public class Math
+        {
+            public int Num1 { get; set; }
+            public int Num2 { get; set; }
+            /*public object Add(int sum)
+            {
+                int sum = Num1 + Num2;
+            }*/
+        }
 
         static void Main(string[] args)
         {
-            
+            //I'm going to run some basic math problems by using user inputs for variables
+            Console.WriteLine("Alright, now let's get into some math coding.");
+            Console.WriteLine("");
+
+            //First up, I'm asking the user for 2 ints that the console adds together
+            Console.WriteLine("First up, let's do some addition. Give me a number:");
+            var Num1 = Console.ReadLine();
+            Console.WriteLine($"Okay, I've got {Num1} down. What's another number?");
+            var Num2 = Console.ReadLine();
+
+            Console.WriteLine($"Okay, that's {Num1} and {Num2}. Add them together...");
+            Console.WriteLine("");
+            //var sum = Math.Sum(Num1, Num2);
+
+            Console.WriteLine($"...and your sum is {Math.Equals(Num1, Num2)}!");
         }
+
+        
     }
 }
 /* Okay, peep this out: this exercise ties in with the Methods prompt from
